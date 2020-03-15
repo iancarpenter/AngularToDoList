38f6ba53b6b38d7a83709906f836d42962dfdd25
iancarpenter <ianhcarpenter@gmail.com>
2020-03-15 21:18:17 +0000

    calling the todo component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ToDoComponent } from './to-do/to-do.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'todo', component: ToDoComponent },
+  { path: '', redirectTo: '/todo', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
