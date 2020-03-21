5425091f498690a1eae41b30ee61061b06dc8ea0
iancarpenter <ianhcarpenter@gmail.com>
2020-03-21 21:44:27 +0000

    Followed google js guide for method formatting

--- src/app/to-do/to-do.component.ts
+++ src/app/to-do/to-do.component.ts
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToDoComponent implements OnInit {
 
-  check = "fa-check-circle";
-  uncheck = "fa-circle-thin";
-  line_through = "lineThrough";
+  readonly check = "fa-check-circle";
+  readonly uncheck = "fa-circle-thin";
+  readonly line_through = "lineThrough";
 
   toDoList: { name: string; id: number; done: boolean; trash: boolean; }[];
   toDoID: number;
@@ -17,29 +17,21 @@ export class ToDoComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-
     this.displayDate();
-
     this.loadToDoList();
   }
 
   // Display the date such as Friday, Mar 20, 2020
   private displayDate() {
-
     const options = { weekday: "long", month: "short", day: "numeric", year: "numeric" };
-
     const today = new Date();
-
     (<HTMLElement>document.getElementById("date")).innerHTML = today.toLocaleDateString("en-UK", options);
-
   }
 
   // recall the todo list if anything is found in the browsers local storage
   // otherwise set up the variables for a new todo list
   private loadToDoList() {
-
     const data = localStorage.getItem("TODO");
-
     if (data) {
       this.toDoList = JSON.parse(data);
       this.toDoID = this.toDoList.length; // set the id to the last one in the list
@@ -50,7 +42,6 @@ export class ToDoComponent implements OnInit {
       this.toDoID = 0;
     }
   }
-
 
   // load the items that the user has not deleted into the user interface
   // When the user deletes an item nothing is removed from the toDoList array just the trash flag is set
@@ -67,19 +58,14 @@ export class ToDoComponent implements OnInit {
   // Display the todo list
   addToDo(toDo: string, id: number, done: boolean, trash: boolean) {        
     const { tick, strikeThrough } = this.formatCompletedItem(done);
-
-    const text = this.toDoliElement(tick, id, strikeThrough, toDo);
-    
+    const text = this.toDoliElement(tick, id, strikeThrough, toDo);    
     this.displayToDoItemOnPage(text);  
   }
 
-  
   // completed items are displayed with a tick and strikethrough
   private formatCompletedItem(done: boolean) {
     const tick = done ? this.check : this.uncheck;
-
-    const strikeThrough = done ? this.line_through : "";
-    
+    const strikeThrough = done ? this.line_through : "";    
     return { tick, strikeThrough };
   }
   
@@ -95,47 +81,34 @@ export class ToDoComponent implements OnInit {
   
   // Insert the new item at the bottom of the UL element
   private displayToDoItemOnPage(text: string) {    
-    const position = "beforeend";
-    
-    const toDolistPosition = document.getElementById("list");
-    
+    const position = "beforeend";  
+    const toDolistPosition = document.getElementById("list");  
     toDolistPosition.insertAdjacentHTML(position, text);
   }
 
   // add or removes the styles used to show if the to do item is complete 
   toggleComplete(element: HTMLElement) {
-
     element.classList.toggle(this.check);
-
     element.classList.toggle(this.uncheck);
-
     element.parentNode.querySelector(".text").classList.toggle(this.line_through);
-
     this.toDoList[element.id].done = this.toDoList[element.id].done ? false : true;      
-
   }
   
   // when the user deletes a to do item, the item is not removed from the list
   // but the trash property is set to true which stops it from being displayed
   removeToDoItem(element: HTMLElement) {
-
     element.parentNode.parentNode.removeChild(element.parentNode);
-
-    this.toDoList[element.id].trash = true;          
-    
+    this.toDoList[element.id].trash = true;            
   }
 
-  // Handles when a user completes or deletes a todo item  
+  // When a user completes or deletes a todo item  
   todoListControls(event: MouseEvent) {
-
     // get the element which the user clicked on
     const element = event.target as HTMLUListElement;
-
     // the value of the job attribute. 
     // This attribute is created by the method addToDo and can have the value
     // complete or delete          
     const elementJOB = element.attributes[1].value;
-
     // route what the user has selected to the appropriate method
     if (elementJOB == "complete") {
       this.toggleComplete(element);    
@@ -148,9 +121,7 @@ export class ToDoComponent implements OnInit {
 
   // Create a new to do item when the user presses the enter key
   addAToDo(event: KeyboardEvent) {
-
     const enterKey = 13;
-
     if (event.keyCode == enterKey) {      
       // get the the new to do item
       let toDoItem = (<HTMLInputElement>document.getElementById("input")).value;          
